42a7be9e8d16d82a5edcb14deb8727879fcf23dc
Sevval Gölbasi <sevval.goelbasi@weareera.com>
2026-06-27 20:12:43 +0200

    Data cleaning and first graphs

---
+++ config.py
@@ -0,0 +1,14 @@
+ISCED_GROUPS = ["ED0-2", "ED3_4", "ED5-8"]
+AGE_BAND = "Y18-64"
+UNIT = "EUR"
+STAT = "MEAN_EI"
+
+COUNTRIES = [
+    "AL", "AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE",
+    "EL", "ES", "FI", "FR", "HR", "HU", "IE", "IS", "IT", "LT",
+    "LU", "LV", "ME", "MK", "MT", "NL", "NO", "PL", "PT", "RO",
+    "RS", "SE", "SI", "SK", "TR", "UK", "XK",
+]
+
+PREMIUM_NUM = "ED5-8"
+PREMIUM_DEN = "ED0-2"
